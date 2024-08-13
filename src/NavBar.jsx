c66eaaa7c12053cad627712f137439a9ae741ce1
Jiahao Yuan <jiahaoyuan55@gmail.com>
2024-08-13 00:28:57 -0400

    papa

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,19 +1,62 @@
-import { useState } from 'react'
-import './NavBar.css'
-
-
+import { useState } from 'react';
+import './NavBar.css';
 
 export default function NavBar() {
+    const [selectedAlgo, setSelectedAlgo] = useState(null);
+    const [animateSelected, setAnimateSelected] = useState(false);
+
+    const handleAlgoChange = (value) => {
+        setSelectedAlgo(value);
+    };
+
+    const handleAnimateChange = () => {
+        setAnimateSelected(!animateSelected);
+    };
+
+    const handleSubmit = () => {
+        console.log('Selected Algorithm:', selectedAlgo);
+        console.log('Animate Selected:', animateSelected);
+        // Here you can add any logic for handling form submission, like API calls
+    };
+
     return (
-        <>
         <div className='bar'>
             <h1 className='Title'>NaviGator</h1>
-            <form >
-                <label className='options'>Dijstrka Algorithim <input type='radio' name='algo' required/></label>
-                <label className='options'>A* Algorithim <input type='radio' name='algo'/></label>
-                <label className='options'>Animate<input type='radio' name='animate'/></label>
+            <form>
+                <label className='options'>
+                    Dijkstra's Algorithm
+                    <input
+                        type='radio'
+                        name='algo'
+                        value='dijkstra'
+                        checked={selectedAlgo === 'dijkstra'}
+                        onChange={() => handleAlgoChange('dijkstra')}
+                        required
+                    />
+                </label>
+                <label className='options'>
+                    A* Algorithm
+                    <input
+                        type='radio'
+                        name='algo'
+                        value='aStar'
+                        checked={selectedAlgo === 'aStar'}
+                        onChange={() => handleAlgoChange('aStar')}
+                    />
+                </label>
+                <label className='options'>
+                    Animate
+                    <input
+                        type='checkbox'
+                        name='animate'
+                        checked={animateSelected}
+                        onChange={handleAnimateChange}
+                    />
+                </label>
+                <button type="button" onClick={handleSubmit}>
+                    Submit
+                </button>
             </form>
         </div>
-        </>
-    )
+    );
 }
