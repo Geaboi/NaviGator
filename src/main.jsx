639999c505bba2fbead04e8c5a879abc8a4bb905
dang1 <dustinn1204@gmail.com>
2024-08-13 00:21:09 -0400

    Added semi-functioning map

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import NavBar from './NavBar.jsx'
-import './index.css'
+import Map from './Map.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+
     
     <NavBar/>
+    <Map></Map>
   </StrictMode>,
 )
